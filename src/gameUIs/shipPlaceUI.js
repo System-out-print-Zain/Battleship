1d7d5511dc422f389229ecd1b4ebbf3d47600725
Zain Mustafa <zz.mhmail@gmail.com>
2023-09-08 18:44:32 -0400

    Reorganize codebase

---
+++ src/gameUIs/shipPlaceUI.js
@@ -0,0 +1,28 @@
+const content = document.querySelector("main");
+
+function displayStartScreen() {}
+
+function displayShipPlaceScreen() {
+  content.innerHTML = 
+  `<div id="grid"></div>
+  <div id="ships"></div>`;
+
+  // Generate grid
+
+  const grid = document.getElementById("grid");
+
+  for (let i = 0; i < 10; i++)
+  {
+    for (let j = 0; j < 10; j++)
+    {
+        const square = document.createElement("div");
+        square.id = `${i * 10 + j}`;
+        square.classList.add("square");
+        grid.appendChild(square);
+    }
+  }
+
+  // Show ships 
+
+
+}

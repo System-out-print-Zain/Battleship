3b171b749eca11eb3088fa914e3e351a77bb322e
Zain Mustafa <zz.mhmail@gmail.com>
2023-09-10 15:03:14 -0400

    Write tests for grid; check if cells property is properly instantiated

---
+++ src/__tests__/grid.test.js
@@ -0,0 +1,31 @@
+import Grid from "../grid";
+
+test("10 x 10 2d array is generated internally.", () => {
+    const testGrid = new Grid();
+
+    expect(testGrid.cells.length === 10 && testGrid.cells[0].length === 10).toBe(true);
+})
+
+test("Each cell is initially empty and unattacked.", () => {
+
+    const testGrid = new Grid();
+
+    let allEmpty = true;
+    let allUnAttacked = true;
+
+    for (let i = 0; i < 10; i ++)
+    {
+        for (let j = 0; j < 10; j ++)
+        {
+            if (testGrid.cells[i][j][0] !== null){
+                allEmpty = false;
+            }
+            if (testGrid.cells[i][j][1] !== "NA"){
+                allUnAttacked = false;
+            }
+        }
+    }
+
+    expect(allEmpty).toBe(true);
+    expect(allUnAttacked).toBe(true);
+})

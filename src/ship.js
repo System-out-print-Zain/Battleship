ddeb40bcc6dd15bbb7d5e62c2802e822a2fe2e51
Zain Mustafa <zz.mhmail@gmail.com>
2023-09-13 12:03:31 -0400

    Implement ship class

--- src/ship.js
+++ src/ship.js
@@ -1,4 +1,15 @@
 
 export default class Ship {
+    constructor(length){
+        this.length = length;
+        this.health = length;
+    }
 
+    hit(){
+        this.health -= 1;
+    }
+
+    destroyed(){
+        return this.health === 0;
+    }
 }

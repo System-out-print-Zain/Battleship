49720054b2dbe6d26817f9060393db23f2553d09
Zain Mustafa <zz.mhmail@gmail.com>
2023-09-15 10:11:34 -0400

    Update tests

--- src/__tests__/player.test.js
+++ src/__tests__/player.test.js
@@ -1,10 +1,15 @@
 import Player from "../player";
 
 describe("Test instantiation", () => {
-    test("The player has a fleet of 5 ships of length 1 to 5", () => {
+    test("The player has a fleet of 5 ships. A carrier (5), battleship (4), cruiser (3), submarine (3), destroyer (2)", () => {
         const player = new Player();
 
-        expect(player.fleet.length).toBe(5);
+        expect(Object.keys(player.fleet)).toEqual(["Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer"]);
+        expect(player.fleet.Carrier.length).toBe(5);
+        expect(player.fleet.Battleship.length).toBe(4);
+        expect(player.fleet.Cruiser.length).toBe(3);
+        expect(player.fleet.Submarine.length).toBe(3);
+        expect(player.fleet.Destroyer.length).toBe(2);
     })
 })
 
@@ -12,14 +17,15 @@ describe("Test fleetDestroyed method", () => {
     test("Return true if fleet is destroyed, false otherwise", () => {
         const player = new Player();
 
-        for (let i = 0; i < player.fleet.length; i++)
-        {
-            player.fleet[i].health = 0;
-        }
+        player.fleet.Carrier.health = 0;
+        player.fleet.Battleship.health = 0;
+        player.fleet.Cruiser.health = 0;
+        player.fleet.Submarine.health = 0;
+        player.fleet.Destroyer.health = 0;
 
         expect(player.fleetDestroyed()).toBeTruthy();
 
-        player.fleet[0].health = 5;
+        player.fleet.Carrier.health = 5;
 
         expect(player.fleetDestroyed()).toBeFalsy();
     })

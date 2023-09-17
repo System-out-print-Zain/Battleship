a337e4968aa41447762b69c84b44b94a62ab2f50
Zain Mustafa <zz.mhmail@gmail.com>
2023-09-16 22:12:56 -0400

    Implement initShips

--- src/gameUIs/shipPlaceUI.js
+++ src/gameUIs/shipPlaceUI.js
@@ -41,21 +41,42 @@ function genShips(player){
   return DOMShips;
 }
 
-export function displayShipPlaceScreen() {
+export function displayShipPlaceScreen(player) {
   const content = document.querySelector("main");
   const grid = genGrid();
   content.appendChild(grid);
 
-  const ships = genShips();
+  const ships = genShips(player);
   content.appendChild(ships);
+}
 
+function initGrid(shipLength){
+
+}
+
+function initShips(){
+  let selectedShip;
+
+  const ships = document.querySelectorAll(".ship");
+  console.log(ships);
+  ships.forEach((ship) => {
+    ship.addEventListener("click", () => {
+      if (selectedShip !== undefined){
+        selectedShip.classList.remove("selected");
+      }
+      selectedShip = ship;
+
+      selectedShip.classList.add("selected");
+      initGrid(selectedShip.length);
+    })
+  })
 }
 
 function initShipPlaceScreen(player){
 
 }
 
-export default function loadShipPlaceScreen(){
-  displayShipPlaceScreen();
+export default function loadShipPlaceScreen(player){
+  displayShipPlaceScreen(player);
   initShipPlaceScreen();
 }

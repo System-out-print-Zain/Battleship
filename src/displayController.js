24bf5ac260fffccf6cb267bab1c251068bcd81d3
Zain Mustafa <zz.mhmail@gmail.com>
2023-09-19 16:41:01 -0400

    Made displayController a gateway to the UI modules

--- src/displayController.js
+++ src/displayController.js
@@ -1,6 +1,23 @@
+import shipPlaceScreenLoader from "./gameUIs/shipPlaceUI";
+import battleScreenLoader from "./gameUIs/battleUI";
+import startScreenLoader from "./gameUIs/startUI";
+import endScreenLoader from "./gameUIs/endUI";
 
-const displayController = {
+const displayController = (() => {
+    function loadShipPlaceScreen(){
+        shipPlaceScreenLoader.load();
+    }
+    function loadBattleScreen(){
+        battleScreenLoader.load();
+    }
+    function loadStartScreen(){
+        startScreenLoader.load();
+    }
+    function loadEndScreen(){
+        endScreenLoader.load();
+    }
 
-}
+    return {loadBattleScreen, loadEndScreen, loadStartScreen, loadShipPlaceScreen};
+})();
 
 export default displayController;
